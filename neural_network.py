3a18324598f0774331d47c4c1e4369f875e1805d
dragmove <dragmove@naver.com>
2018-05-31 02:51:03 +0900

    feat: Add iwh, who matrics

--- neural_network.py
+++ neural_network.py
@@ -1,3 +1,6 @@
+import numpy as np
+import scipy.special
+
 class NeuralNetwork:
     # init neural network
     def __init__(self, input_nodes, hidden_nodes, output_nodes, learning_rate):
@@ -5,7 +8,14 @@ class NeuralNetwork:
         self.hnodes = hidden_nodes
         self.onodes = output_nodes
 
+        # weight input to hidden, hidden to output
+        self.w_ih = np.random.normal(0.0, pow(self.hnodes, -0.5), (self.hnodes, self.inodes))
+        self.w_ho = np.random.normal(0.0, pow(self.onodes, -0.5), (self.onodes, self.hnodes))
+
         self.lr = learning_rate
+
+        # sigmoid function
+        self.activation_function = lambda x: scipy.special.expit(x)
         pass
 
     # train neural network
